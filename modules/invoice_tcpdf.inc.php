c8211f78a9897ab51e2bb714da28625d6e6b9c0b
Lukasz Wojton <lukasz@atomit.pl>
2015-02-05 23:46:46 +0100

    Zmiany w FV, usunieto druk do przelewu

--- modules/invoice_tcpdf.inc.php
+++ modules/invoice_tcpdf.inc.php
@@ -430,11 +430,11 @@ function invoice_buyer() {
 	$pdf->SetFont('arial', 'B', 10);
 	$pdf->writeHTMLCell(80, '', 125, 50, $postbox, 0, 1, 0, true, 'L');
 
-	$pin = '<b>' . trans('Customer ID: $a', sprintf('%04d', $invoice['customerid'])) . '</b><br>';
-	$pin .= '<b>PIN: ' . sprintf('%04d', $invoice['customerpin']) . '</b><br>';
-
-	$pdf->SetFont('arial', 'B', 8);
-	$pdf->writeHTMLCell('', '', 125, $oldy + round(($y - $oldy) / 2), $pin, 0, 1, 0, true, 'L');
+//	$pin = '<b>' . trans('Customer ID: $a', sprintf('%04d', $invoice['customerid'])) . '</b><br>';
+//	$pin .= '<b>PIN: ' . sprintf('%04d', $invoice['customerpin']) . '</b><br>';
+//
+//	$pdf->SetFont('arial', 'B', 8);
+//	$pdf->writeHTMLCell('', '', 125, $oldy + round(($y - $oldy) / 2), $pin, 0, 1, 0, true, 'L');
 
 	$pdf->SetY($y);
 }
@@ -576,11 +576,11 @@ function invoice_body_ft0100() {
 	invoice_expositor();
 	invoice_footnote();
 	/* draw FT-0100 form */
-	invoice_simple_form_draw();
-	invoice_main_form_draw();
+	//invoice_simple_form_draw();
+	//invoice_main_form_draw();
 	/* fill FT-0100 form */
-	invoice_simple_form_fill();
-	invoice_main_form_fill();
+	//invoice_simple_form_fill();
+	//invoice_main_form_fill();
 	if (!$invoice['fullnumber'])
 	    $docnumber = docnumber($invoice['number'], $invoice['template'], $invoice['cdate']);
 	else
